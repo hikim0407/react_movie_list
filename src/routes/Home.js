3781097e4d9dc56ffadfa3e75664295af5a45caf
hikim <kimhyoim23@gmail.com>
2022-04-27 01:27:53 +0900

    스타일 적용

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
     const getMovies = async() => {
       const json = await (
-        await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
+        await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&order_by=desc")
       ).json();
       
       setMovies(json.data.movies);
@@ -18,14 +19,20 @@ function Home() {
     }, []);
     
     return (
-      <div>
-        {loading ? <h1>loading...</h1> : (
-          <div>{movies.map(movie => (
+      <div className={styles.container}>
+        {loading ? (
+          <div className={styles.loader}>
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div className={styles.movies}>
+            {movies.map(movie => (
             <Movie 
             key={movie.id}
             id={movie.id}
             coverImg={movie.medium_cover_image} 
             title={movie.title} 
+            year={movie.year} 
             summary={movie.summary} 
             genres={movie.genres} 
             />)
